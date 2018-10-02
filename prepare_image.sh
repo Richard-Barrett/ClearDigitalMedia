3615fb12e6317a63e9581490d352e3c59f6e1426
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-02 08:56:47 -0500

    Update prepare_image.sh

--- prepare_image.sh
+++ prepare_image.sh
@@ -9,6 +9,7 @@
 # https://www.howtoforge.com/tutorial/how-to-install-and-use-iostat-on-ubuntu-1604/
 # http://www.codebind.com/linux-tutorials/install-curl-ubuntu-16-04/
 # https://askubuntu.com/questions/833863/how-to-install-animation-addon-plugin-of-compiz-in-ubuntu-16-04
+# https://broadsign.com/docs/broadsign-control/12-1/install-player-on-linux.html
 
 
 # Tweaking Documentation
@@ -35,8 +36,8 @@ sudo apt-get -f install wget -y
 sudo apt-get -f install vim -y
 sudo apt-get -f install compizconfig-settings-manager compiz-plugins-extra -y
 sudo apt-get -f install synaptic -y
-  sudo add-apt-repository ppa:appgrid/stable -y
-  sudo apt-get update -y
+sudo add-apt-repository ppa:appgrid/stable -y
+sudo apt-get update -y
 sudo apt-get -f install appgrid -y
 
 
@@ -49,8 +50,8 @@ mkdir Clear_Digital_Media
 cd 
 
 
-# Script will curl and download Teamviewer and install into Clear Digital Media under Apps
-# ========================================================================================
+# Script will curl and download Teamviewer and install into tmp
+# ==============================================================
 
 cd /tmp && wget https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc
 
@@ -67,29 +68,25 @@ cd
 
 # Script will curl and install broadsign player under broadsign package
 # =====================================================================
-cd /Clear_Digital_Media/Broadsign/ && wget 
+cd /Clear_Digital_Media/Broadsign/ 
+
+wget --user <username> --password <pass> http://portal.broadsign.com
 
 sudo apt-key add <BROADSIGN PACKAGE>
+
+sudo sh -c 'echo "deb http://portal.broadsign.com/deb stable main" >> /etc/apt/sources.list.d/broadsign.list'
+sudo sh -c 'echo "deb http://portal.broadsign.com/deb preview main" >> /etc/apt/sources.list.d/broadsign.list'
+
+sudo apt update
+sudo apt install broadsign -y
 
 # Script will check for dependencies and push new commits
 # =======================================================
 sudo apt-get install -f -y
 
-# script will edit and invoke bash and print an alias onto .bashrc in wd
-# ======================================================================
-
-# Script will set up crontab for broadsign
-# ========================================
-crontab -e
-  [2]
-    cat '@reboot sleep 10 && broasign
-
 
 # Script will move all of the necessary applications onto the desktop 
 # ===================================================================
 
-
-# Script will disable gnome-unity bar and/or set Compiz-Copnfig-Manager Opacity to 0.0000
-# =======================================================================================
 
 #Done
