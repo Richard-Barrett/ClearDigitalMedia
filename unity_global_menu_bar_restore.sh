5309cf82bfa5cc714dd752213125fa693bc9e96d
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-02 11:14:56 -0500

    Create unity_global_menu_bar_restore.sh
    When this ran it will provide the use the ability to reactivate unity tweak bar.

---
+++ unity_global_menu_bar_restore.sh
@@ -0,0 +1,5 @@
+#1/bin/bash
+sudo mv /usr/lib/x86_64-linux-gnu/compiz/libunityshell.so.disabled /usr/lib/x86_64-linux-gnu/compiz/libunityshell.so
+sudo mv /usr/share/compiz/unityshell.xml.disabled /usr/share/compiz/unityshell.xml
+reboot
+#done
