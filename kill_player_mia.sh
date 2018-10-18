3537a63aef1f37943551d5bfdcee41c57b59a65d
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-18 06:18:35 -0500

    Create kill_player_mia.sh

---
+++ kill_player_mia.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Created by Richard Barrett
+# Date October 18, 2018
+# Properrty of Clear Digital Media
+
+# Define Script Variables
+# =======================
+
+# Script will detect if player has gone MIA
+# =========================================
+
+
+
+
+
+# If player is MIA, then script kills player process
+# ==================================================
+
+
+
+
+#Done
