bfa2206b2794163be6a257b9df7ee652c4d13e6f
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-19 12:34:35 -0500

    Update finish_script.sh

--- finish_script.sh
+++ finish_script.sh
@@ -7,10 +7,14 @@
 sudo -i
 sudo systemctl start teamviewerd.service
 sudo teamviewer daemon enable
-su cleardigitalmedia
 
 # Enable Broadsign Daemon
 
+# Script changes to root user and forces an update/upgrade to system components
+# =============================================================================
+apt-get update -y
+apt-get upgrade -y
+sudo su cleardigitalmedia
 
 
 #Done
