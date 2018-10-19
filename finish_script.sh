0987bb6115a271c0d31af1dfa73d95f56b2eaba3
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-19 12:29:52 -0500

    Update finish_script.sh

--- finish_script.sh
+++ finish_script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Script will start start-up programs and will enable daemons per program
+# =======================================================================
+
+# Enable Teamviewer Daemon
+sudo -i
+sudo systemctl start teamviewerd.service
+sudo teamviewer daemon enable
+su cleardigitalmedia
+
+# Enable Broadsign Daemon
+
 
 
 #Done
