dc329450462230dcdac56abbca37108c314c16ee
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-10-30 09:10:58 -0500

    Create client_data_push.sh

---
+++ client_data_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# =======================================
+# Created by Richard Barrett
+# Date: 10/30/2018 ; Version: 1.0
+# =======================================
+
+# NOTES**
+# =====================================================
+# Script will check in with Nagios Server
+# Script will populate Dynamic Tables to MySQL Server
+# Script will reboot the player at midnight per cronjob
+# =====================================================
+
+# Nagios Push
+
+# MySQL Populate Data Push
+
+# Reboot
+reboot
+
+#done
