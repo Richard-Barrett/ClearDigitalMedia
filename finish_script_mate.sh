4eb78acd89deaa7943d0d6bd56228bcb835eef54
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2018-11-19 09:36:21 -0600

    Update finish_script_mate.sh

--- finish_script_mate.sh
+++ finish_script_mate.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+# Script will start start-up programs and will enable daemons per program
+# =======================================================================
+
+# Enable Teamviewer Daemon
+sudo systemctl start teamviewerd.service
+sudo teamviewer daemon enable
+
+# Enable Broadsign Daemon
+
+# Script changes to root user and forces an update/upgrade to system components
+# =============================================================================
+apt-get update -y
+apt-get upgrade -y
+
+# Script will place the finishing touches like Crontab and Logrotate Utilizing Expect
+# ===================================================================================
+cd Desktop/Clear_Digital_media
+sudo cp logrotate.conf /etc/logrotate.conf
+cd ~
+
+
+#Done
 
