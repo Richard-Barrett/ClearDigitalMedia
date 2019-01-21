795373b30768daa3c2cb099c09bf6ab0e2753414
Cleardigitalmedia <45104173+Cleardigitalmedia@users.noreply.github.com>
2019-01-20 19:57:59 -0600

    Create CDM Stop.sh

---
+++ CDM Stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Created by Richard Barrett
+# Date 01/20/2019
+# Version 1.3
+
+# Scrit restores the bash session to then sources it to stop the player
+=======================================================================
+/bin/cp /etc/skel/.bashrc ~/
+source .bashrc
+
